d6e33d1477c221f4bb1b19ee93c9f288c92fc89d
Shrey Nagda <shreynagda2714@gmail.com>
2025-01-29 20:08:27 +0530

    Landing page and other required sections created

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen gap-4">
-      404 | Page Not Found
+    <div className="flex flex-col items-center justify-center min-h-screen gap-4 bg-light-primary">
+      <div className="text-black text-xl md:text-2xl">Coming Soon...</div>
       <Link href={"/"} className="bg-black text-white p-2 rounded-md">
         Back to Home
       </Link>
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import Logo from "@/Components/Logo";
-import { noto_serif_display } from "@/lib/fonts";
+import { inter, noto_serif_display } from "@/lib/fonts";
+import { AnimatePresence, motion } from "framer-motion";
 import { getFeaturedProducts, getProducts } from "@/sanity/sanity-utils";
+import About from "@/Components/About";
+import Hero from "@/Components/Hero";
 
 export default async function Home() {
   // Fetching data from Sanity
@@ -8,27 +11,10 @@ export default async function Home() {
   // const featuredProducts = await getFeaturedProducts();
   return (
     <div>
-      <section
-        className={
-          "flex flex-col items-center pt-32 text-white h-[calc(100vh-80px)] " +
-          noto_serif_display.className
-        }
-      >
-        <div className="text-amber-400 tracking-wide text-lg md:text-xl ">
-          Kaanchwala & Sons
-        </div>
-        <div className="text-4xl text-center tracking-wider md:text-5xl max-w-sm mx-auto">
-          Where Vision Meets Style
-        </div>
-      </section>
-      <section
-        className={
-          "flex flex-col items-center justify-center bg-light-primary text-black h-[95vh] gap-2 text-2xl " +
-          noto_serif_display.className
-        }
-      >
-        Coming Soon..
-      </section>
+      <>
+        <Hero />
+        <About />
+      </>
     </div>
   );
 }
